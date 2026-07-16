183e5b8b3a5ac50b865147ca1b7ce423bdbc7d92
Stygian <hi@how.are.you>
2026-07-16 12:54:46 +0300

    remove make stuff

--- build/CMakeFiles/vklayer_understyx_wotlk.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for vklayer_understyx_wotlk.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/vklayer_understyx_wotlk.dir/src/main.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/vklayer_understyx_wotlk.dir/src/main.cpp.o: \
- /home/runner/work/Vk-edgy-wotlk/Vk-edgy-wotlk/src/main.cpp \
- /usr/include/stdc-predef.h
